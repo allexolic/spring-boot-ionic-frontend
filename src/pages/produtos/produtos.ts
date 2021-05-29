591c510944530485a2dc86e9219054803b7feff5
Alexandre Oliveira Curvelo <allexolic@gmail.com>
2021-05-29 12:05:47 -0300

    Exibindo imagem dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -23,6 +24,7 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrls();
       },
         error => {});
 
@@ -40,6 +42,17 @@ export class ProdutosPage {
         preco: 100.00
       }
     ] */
+  }
+
+  loadImageUrls(){
+    for (var i=0; i<this.items.length; i++){
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
+    }
   }
 
 }
