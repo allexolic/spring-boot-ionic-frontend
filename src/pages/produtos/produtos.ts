032592692b2402d8b332373e1f348321723a3040
Alexandre Oliveira Curvelo <allexolic@gmail.com>
2021-05-29 11:13:58 -0300

    Listando produtos por categoria

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { ProdutoService } from '../../services/domain/produto.service';
 
 @IonicPage()
 @Component({
@@ -11,11 +12,22 @@ export class ProdutosPage {
 
   items: ProdutoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public produtoService : ProdutoService) {
   }
 
   ionViewDidLoad() {
     //console.log('ionViewDidLoad ProdutosPage');
+    let categoria_id = this.navParams.get('categoria_id');
+    this.produtoService.findByCategoria(categoria_id)
+      .subscribe(response => {
+        this.items = response['content'];
+      },
+        error => {});
+
+/*
+    //MOCK DATA
     this.items = [
       {
         id: "1",
@@ -27,7 +39,7 @@ export class ProdutosPage {
         nome: 'Teclado',
         preco: 100.00
       }
-    ]
-  };
+    ] */
+  }
 
 }
