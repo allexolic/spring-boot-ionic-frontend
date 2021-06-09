f989457f97be571c274e3251338a1f6c804b8701
Alexandre Oliveira Curvelo <allexolic@gmail.com>
2021-06-09 12:12:32 -0300

    Buscando enderecos do banco de dados

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { StorageService } from '../../services/storage.service';
 
 @IonicPage()
 @Component({
@@ -11,28 +13,27 @@ export class PickAddressPage {
 
   items: EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public storage: StorageService,
+              public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Conde de Baependi",
-        numero: "54",
-        complemento: "apto 301",
-        bairro: "Flamengo",
-        cep: "22231140",
-        cidade: {
-          id: "1",
-          nome: "Rio de Janeiro",
-          estado: {
-            id: "1",
-            nome: "Rio de Janeiro"
+    let localUser = this.storage.getLocalUser();
+    if(localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items = response['enderecos'];
+        },
+        error => {
+          if(error.status == 403){
+            this.navCtrl.setRoot('HomePage');
           }
-        }
-      }
-    ];
+        });
+    }else {
+      this.navCtrl.setRoot('HomePage');
+    }
   }
 
 }
