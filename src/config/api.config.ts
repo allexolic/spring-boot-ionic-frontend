a483f8b70eec6a26feba61eaead73b6ef23197ed
Alexandre Oliveira Curvelo <allexolic@gmail.com>
2021-06-11 13:09:17 -0300

    testando app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    //baseUrl: "http://localhost:8080",
+    baseUrl: "https://curso-rest-api-spring-ionic.herokuapp.com",
     bucketBaseUrl: "https://curso-springboot-v1.s3-sa-east-1.amazonaws.com"
 }
